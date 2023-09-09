ccf964cedeac1ca076bdb4c2eed9ed4c4e884eda
Iván Berruet <ivanberruet95@gmail.com>
2023-09-08 21:08:18 -0300

    Clone test add

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -9,7 +9,7 @@ export default function Rules() {
 				<li className='list-inside list-disc marker:text-xl py-1 pt-2'>Modalidad de juego Texas Hold'em, dos cartas en mano (preflop) y 5 en mesa (flop, turn y river).</li>
 				<li className='list-inside list-disc marker:text-xl py-1'>Entrada $3.000.</li>
 				<li className='list-inside list-disc marker:text-xl py-1'>1 reingreso por jugador permitido hasta antes de quedar solo 2 jugadores en juego ($3.000 el reingreso).</li>
-				<li className='list-inside list-disc marker:text-xl py-1'>Inician con 1500 en fichas cada jugador.</li>
+				<li className='list-inside list-disc marker:text-xl py-1'>Inician con aproximadamente 1500 en fichas cada jugador.</li>
 				<li className='list-inside list-disc marker:text-xl py-1'>Incremento de ciegas cada 15 minutos.</li>
 				<li className='list-inside list-disc marker:text-xl py-1'>El pozo se conforma con la entrada de los jugadores y se le incorpora cada reingreso que se efectúe.</li>
 				<li className='list-inside list-disc marker:text-xl py-1'>1er puesto 85% del pozo.</li>
